4e4285ca2fc5e4b646be15489be277644455eef9
Im-Tae <dlaxo2420@naver.com>
2020-06-05 12:44:52 +0900

    Spring 1

---
+++ Spring/1/src/main/kotlin/com/leaf/demo/IndexController.kt
@@ -0,0 +1,14 @@
+package com.leaf.demo
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@EnableAutoConfiguration(exclude = [DataSourceAutoConfiguration::class])
+class IndexController {
+
+    @GetMapping(path = ["/"])
+    fun index() = "Hello Spring!"
+}
