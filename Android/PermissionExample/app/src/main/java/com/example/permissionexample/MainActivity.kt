78555764da06cc928b846eb21567bd48558f3ebb
Im-Tae <dlaxo2420@naver.com>
2020-08-19 14:31:35 +0900

    Permission

---
+++ Android/PermissionExample/app/src/main/java/com/example/permissionexample/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.example.permissionexample
+
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    private val permissionList = arrayOf(
+            Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.READ_CONTACTS,
+            Manifest.permission.WRITE_CONTACTS,
+            Manifest.permission.SEND_SMS,
+            Manifest.permission.RECEIVE_SMS
+    )
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        checkPermission()
+    }
+
+    private fun checkPermission() {
+
+        if (Build.VERSION.SDK_INT > Build.VERSION_CODES.M) {
+
+            for(permission in permissionList) {
+
+                if (checkCallingOrSelfPermission(permission) == PackageManager.PERMISSION_DENIED) {
+                    requestPermissions(permissionList, 0)
+                }
+            }
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    }
+}
