2b4cd1984f0f3588c5255a6f20f08eb80c133142
Im-Tae <dlaxo2420@naver.com>
2020-08-21 16:14:05 +0900

    start other app

---
+++ Android/StartOtherAppExample/app/src/main/java/com/example/startotherappexample/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.startotherappexample
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        button.setOnClickListener {
+
+            val intent = packageManager.getLaunchIntentForPackage("com.android.chrome")
+            startActivity(intent)
+        }
+    }
+}
